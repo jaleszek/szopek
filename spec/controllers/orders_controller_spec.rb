ba96c0e215bcb0824d7180be3205ec3c64d2b36b
Leszek <andrukanisleszek@gmail.com>
2014-02-03 11:50:56 +0100

    Add OrdersController spec to deal with payments redirection

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,34 +1,34 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe OrdersController do
-#   before{ load("#{Rails.root}/lib/seeds/populate_order_page.rb") }
+describe OrdersController do
+  before{ load("#{Rails.root}/lib/seeds/populate_order_page.rb") }
 
-#   let(:cart) { Cart.last }
-#   let(:location) { 'http://some_payment_service.com' }
+  let(:cart){ Cart.last }
+  let(:location){ 'http://some_payment_service.com' }
 
-#   describe '#create' doz
-#     context 'failure' do
-#       before do
-#         stub_request(:any, Settings.payment_endpoint_address).
-#         to_return(:status => 500)
-#       end
+  describe '#create' do
+    context 'failure' do
+      before do
+        stub_request(:any, Settings.payment_endpoint_address).
+        to_return(:status => 500)
+      end
 
-#       it 'redirects to the new order page' do
-#         post :create, {"payment_method"=>"visa", "cart_id" => cart.id }
-#         expect(response).to redirect_to(new_cart_order_path(cart))
-#       end
-#     end
+      it 'redirects to the new order page' do
+        post :create, { "payment_method"=>"visa", "cart_id" => cart.id }
+        expect(response).to redirect_to(new_cart_order_path(cart))
+      end
+    end
 
-#     context 'success' do
-#       before do
-#         stub_request(:any, Settings.payment_endpoint_address).
-#         to_return(:status => 302, :headers => { 'Location' => location })
-#       end
+    context 'success' do
+      before do
+        stub_request(:any, Settings.payment_endpoint_address).
+        to_return(:status => 302, :headers => { 'Location' => location })
+      end
 
-#       it 'redirects to external service' do
-#         post :create, {"payment_method"=>"visa", "cart_id" => cart.id }
-#         expect(response).to redirect_to(location)
-#       end
-#     end
-#   end
-# end
+      it 'redirects to external service' do
+        post :create, { "payment_method"=>"visa", "cart_id" => cart.id }
+        expect(response).to redirect_to(location)
+      end
+    end
+  end
+end
